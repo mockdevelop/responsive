f12d48b694e8f47a134d518871b801d38801bf5b
gaspar <gaspar@pangeaconnect.com>
2015-07-09 11:10:00 -0400

    testing

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// CSS-only tabs made easy with Sass.
+
+// This demonstrates how I use the float isolation method, along with opacity and z-index, to create a responsive tab container. 
+
+// - Container wraps all tabs dynamically
+// - Container size remains equal across tabs
+// - No JS height calculations
+// - The entire thing is responsive
+
+// You can use the `display` property instead if you want the container to change sizes depending on its content.
